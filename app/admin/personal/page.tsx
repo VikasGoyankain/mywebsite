99ceac4b80df2cf066a7bcf990d159323c4deef6
Vikas <vikasgoyanka.in@gmail.com>
2025-06-21 17:47:08 +0530

    Add admin section for personal area management

---
+++ app/admin/personal/page.tsx
@@ -0,0 +1,5 @@
+import { PersonalRegistrationForm } from "@/components/personal/PersonalRegistrationForm";
+
+export default function PersonalManagementPage() {
+  return <PersonalRegistrationForm />;
+} 
